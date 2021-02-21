1c0ec06453f8146a6d326a974449a5393af461f7
David <david87124@gmail.com>
2021-02-22 01:58:38 +0800

    implement select, crop image

--- app/src/main/java/com/davidchen/drawimg/MainActivity.kt
+++ app/src/main/java/com/davidchen/drawimg/MainActivity.kt
@@ -1,11 +1,69 @@
 package com.davidchen.drawimg
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.davidchen.drawimg.databinding.ActivityMainBinding
+import com.yalantis.ucrop.UCrop
+import lv.chi.photopicker.PhotoPickerFragment
+import timber.log.Timber
+import java.io.File
+import java.util.*
+import kotlin.collections.ArrayList
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), PhotoPickerFragment.Callback {
+
+    lateinit var v: ActivityMainBinding
+    lateinit var preview: PreviewFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        v = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(v.root)
+
+        Timber.plant(Timber.DebugTree())
+
+        preview = PreviewFragment.newInstance()
+        supportFragmentManager.beginTransaction()
+            .add(R.id.root, preview)
+            .commit()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == UCrop.REQUEST_CROP) {
+            if (data != null) {
+                val resultUri = UCrop.getOutput(data)
+                resultUri?.let { preview.setImage(it) }
+            }
+        } else if (resultCode == UCrop.RESULT_ERROR) {
+            val cropError = data?.let { UCrop.getError(it) }
+            Timber.e("UCrop error: ${cropError?.message}")
+        }
+    }
+
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount == 0) {
+            finish()
+        } else {
+            supportFragmentManager.popBackStack()
+        }
+    }
+
+    override fun onImagesPicked(photos: ArrayList<Uri>) {
+        Timber.d(photos[0].lastPathSegment)
+        val uri = photos[photos.size - 1]
+        val now = Calendar.getInstance().time.toString()
+        val fileName = uri.lastPathSegment + now
+        val destination = File(cacheDir, fileName)
+        val options: UCrop.Options = UCrop.Options()
+        options.setFreeStyleCropEnabled(true)
+
+        Timber.d("destination: $destination")
+        UCrop.of(uri, Uri.fromFile(destination))
+            .withOptions(options)
+            .start(this)
     }
 }
---
+++ app/src/main/java/com/davidchen/drawimg/PreviewFragment.kt
@@ -0,0 +1,75 @@
+package com.davidchen.drawimg
+
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.davidchen.drawimg.databinding.FragmentPreviewBinding
+import lv.chi.photopicker.ChiliPhotoPicker
+import lv.chi.photopicker.PhotoPickerFragment
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [PreviewFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class PreviewFragment : Fragment() {
+
+    lateinit var v: FragmentPreviewBinding
+    lateinit var photoUri: Uri
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let { }
+        ChiliPhotoPicker.init(
+            loader = GlideImageLoader(),
+            authority = "com.davidchen.drawimg"
+        )
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_preview, container, false)
+        v = FragmentPreviewBinding.bind(view)
+
+        v.ivImage.setOnClickListener {
+            PhotoPickerFragment.newInstance(
+                multiple = false,
+                allowCamera = false,
+                theme = R.style.ChiliPhotoPicker_Dark
+            ).show(parentFragmentManager, "photo")
+        }
+
+        v.btEditImg.setOnClickListener {
+            TODO("draw fragment implement")
+        }
+
+        return v.root
+    }
+
+    fun setImage(photo: Uri) {
+        photoUri = photo
+        Glide.with(context)
+            .load(photo)
+            .asBitmap()
+            .fitCenter()
+            .into(v.ivImage)
+        v.btEditImg.visibility = View.VISIBLE
+        v.ivAdd.visibility = View.INVISIBLE
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            PreviewFragment().apply {
+                arguments = Bundle().apply { }
+            }
+    }
+}
